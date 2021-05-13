87b13936089ae2fef5e8064864ba9aaeef5ac9c6
simpleoier <netnetchangxk@gmail.com>
2021-05-12 20:34:03 -0400

    move CE loss weight to loss_weights.py

--- espnet2/hybrid_asr/espnet_model.py
+++ espnet2/hybrid_asr/espnet_model.py
@@ -22,6 +22,7 @@ from espnet2.asr.encoder.abs_encoder import AbsEncoder
 from espnet2.asr.frontend.abs_frontend import AbsFrontend
 from espnet2.asr.preencoder.abs_preencoder import AbsPreEncoder
 from espnet2.asr.specaug.abs_specaug import AbsSpecAug
+from espnet2.hybrid_asr.loss_weights import normedWeights
 from espnet2.layers.abs_normalize import AbsNormalize
 from espnet2.torch_utils.device_funcs import force_gatherable
 from espnet2.train.abs_espnet_model import AbsESPnetModel
@@ -33,138 +34,6 @@ else:
     @contextmanager
     def autocast(enabled=True):
         yield
-
-label_samples=[
-    10888174,
-    4239994,
-    3764906,
-    2832657,
-    2355986,
-    1407264,
-    1465425,
-    1283446,
-    1309389,
-    1198724,
-    1076269,
-    838217,
-    770442,
-    746908,
-    675613,
-    602303,
-    599189,
-    666542,
-    564322,
-    563007,
-    491817,
-    502596,
-    573153,
-    504542,
-    489471,
-    483707,
-    443495,
-    458990,
-    417451,
-    488922,
-    395639,
-    435645,
-    420558,
-    421057,
-    454435,
-    417093,
-    394462,
-    369486,
-    370035,
-    344336,
-    373462,
-    355162,
-    337502,
-    310203,
-    322019,
-    331866,
-    280456,
-    325277,
-    278009,
-    314916,
-    314502,
-    309215,
-    271945,
-    279476,
-    268366,
-    273357,
-    282746,
-    291919,
-    277133,
-    288849,
-    280442,
-    260575,
-    278289,
-    263387,
-    260539,
-    276940,
-    265651,
-    266467,
-    268625,
-    265779,
-    259352,
-    264321,
-    233377,
-    248513,
-    249558,
-    247734,
-    238930,
-    237114,
-    237972,
-    227185,
-    233741,
-    226440,
-    230379,
-    228354,
-    223397,
-    215155,
-    218371,
-    211654,
-    216080,
-    212899,
-    208194,
-    195754,
-    190549,
-    187444,
-    177319,
-    153806,
-    145137,
-    125141,
-    124866,
-    119975,
-    104381,
-    100514,
-    84695,
-    40134,
-    21981,
-    22182,
-    8175,
-    758,
-    69,
-    66,
-    28,
-    4,
-    1,
-    1,
-    1,
-    1,
-] 
-# option 1: just give some specific weights
-# normedWeights = [1 - (x / sum(label_samples)) for x in label_samples]
-# normedWeights[0:3] = [0.05, 0.08, 0.1]
-# print('weights:', normedWeights)
-
-# option 2: (log2(x+100)) ** 2  
-normedWeights = torch.FloatTensor(label_samples)
-normedWeights = torch.log2(normedWeights + 100)
-normedWeights = normedWeights * normedWeights
-normedWeights = 1 / (normedWeights / normedWeights.sum())
-normedWeights = 100 * normedWeights / normedWeights.sum()
-
-print('weights:', normedWeights)
 
 
 def pick_longer_utt(ref1,ref2):
@@ -232,26 +101,15 @@ class ESPnetHybridASRModel(AbsESPnetModel):
                 torch.nn.Linear(encoder.output_size(), vocab_size),
                 torch.nn.Linear(encoder.output_size(), vocab_size),
             ])
-        # self.cross_entropy = LabelSmoothingLoss(
-        #     size=vocab_size,
-        #     padding_idx=ignore_id,
-        #     smoothing=lsm_weight,
-        #     normalize_length=length_normalized_loss,
-        #     criterion=torch.nn.KLDivLoss(reduce=False, reduction="none"),
-        #     reduce=False,
-        # )
-        self.cross_entropy = torch.nn.CrossEntropyLoss(weight=torch.FloatTensor(normedWeights), ignore_index=ignore_id, reduce=False, reduction="none")
+        self.cross_entropy = torch.nn.CrossEntropyLoss(
+            weight=torch.FloatTensor(normedWeights),
+            ignore_index=ignore_id,
+            reduce=False,
+            reduction="none"
+        )
         self.dropout_rate = 0.0
         self.cut_begin_end = False
         self.only_longer_ref = False
-        # self.decoder = decoder
-        # self.rnnt_decoder = rnnt_decoder
-        # self.criterion_att = LabelSmoothingLoss(
-        #     size=vocab_size,
-        #     padding_idx=ignore_id,
-        #     smoothing=lsm_weight,
-        #     normalize_length=length_normalized_loss,
-        # )
 
         if report_cer or report_wer:
             self.error_calculator = ErrorCalculator(
@@ -296,7 +154,6 @@ class ESPnetHybridASRModel(AbsESPnetModel):
         )
         batch_size = speech_mix.shape[0]
 
-
         # for data-parallel
         targets = [
             phn_ref1[:, : phn_ref1_lengths.max()],
@@ -314,47 +171,7 @@ class ESPnetHybridASRModel(AbsESPnetModel):
 
         # 1. Encoder
         encoder_out, encoder_out_lens = self.encode(speech_mix, speech_mix_lengths) # n_spk * (bs, lens, enc_dim)
-        # if isinstance(self.ce_lo, torch.nn.ModuleList):
-        if isinstance(encoder_out, list):
-            if encoder_out[0].shape[1] < phn_ref1.shape[1]:
-                phn_ref1 = phn_ref1[:, :encoder_out[0].shape[1]].contiguous()
-                phn_ref2 = phn_ref2[:, :encoder_out[0].shape[1]].contiguous()
-            elif encoder_out[0].shape[1] > phn_ref1.shape[1]:
-                encoder_out[0] = encoder_out[0][:, :phn_ref1.shape[1]].contiguous()
-                encoder_out[1] = encoder_out[1][:, :phn_ref1.shape[1]].contiguous()
-
-            if self.cut_begin_end:
-                assert encoder_out[0].shape[1]==phn_ref1.shape[1]==phn_ref2.shape[1], (encoder_out[0].shape,phn_ref1.shape,phn_ref2.shapie)
-                cut_begin = int(0.2* phn_ref1_lengths.max())
-                cut_len = int(0.5*phn_ref1_lengths.max())
-                cut_end = int(0.2* phn_ref1_lengths.max()) + cut_len
-                encoder_out=[enc[:,cut_begin:cut_end] for enc in encoder_out]
-                encoder_out_lens= [torch.ones_like(enc) * int(cut_len) for enc in encoder_out_lens]
-                targets = [
-                    phn_ref1[:, cut_begin : cut_end],
-                    phn_ref2[:, cut_begin : cut_end],
-                ]
-                targets_lengths = encoder_out_lens
-
-
-            ys_hats = [
-                self.ce_lo(F.dropout(enc_out, p=self.dropout_rate)) for enc_out in encoder_out
-            ] # n_spk * (bs, lens, proj)
-            ys_hats_lengths = encoder_out_lens
-        else:
-            if encoder_out.shape[1] < phn_ref1.shape[1]:
-                phn_ref1 = phn_ref1[:, :encoder_out.shape[1]].contiguous()
-                phn_ref2 = phn_ref2[:, :encoder_out.shape[1]].contiguous()
-            elif encoder_out.shape[1] > phn_ref1.shape[1]:
-                encoder_out = encoder_out[:, :phn_ref1.shape[1]].contiguous()
-
-            ys_hats = [
-                lo(F.dropout(encoder_out, p=self.dropout_rate)) for lo in self.ce_lo
-            ]
-            ys_hats_lengths = [encoder_out_lens, encoder_out_lens]
-
-        # print("ys_hats:", ys_hats[0].shape, ys_hats[1][0].max(-1)[1][800:820].data.cpu().numpy())
-
+        ys_hats, ys_hats_lengths = self._compute_output_layer(encoder_out, encoder_out_lens, phn_ref1, phn_ref2)
         if self.only_longer_ref:
             loss_ce = self._calc_ce_loss(ys_hats[0],ys_hats_lengths[0],targets,targets_lengths)
             all_ys_hats = ys_hats[0]
@@ -405,8 +222,6 @@ class ESPnetHybridASRModel(AbsESPnetModel):
             all_targets,
             ignore_label=self.ignore_id,
         )
-        # print("targets:", all_targets[1][800:820].data.cpu().numpy())
-        # print("predict:", all_ys_hats.max(-1)[1][1][800:820].data.cpu().numpy(), "\n")
 
         loss = loss_ce
 
@@ -414,9 +229,6 @@ class ESPnetHybridASRModel(AbsESPnetModel):
             loss=loss.detach(),
             acc=acc_ce,
         )
-
-        # for name,param in self.ce_lo.named_parameters():
-            # print(name,param)
 
         # force_gatherable: to-device and to-tensor if scalar for DataParallel
         loss, stats, weight = force_gatherable((loss, stats, batch_size), loss.device)
@@ -503,6 +315,55 @@ class ESPnetHybridASRModel(AbsESPnetModel):
             feats, feats_lengths = speech, speech_lengths
         return feats, feats_lengths
 
+    def _compute_output_layer(
+        self,
+        encoder_out,
+        encoder_out_lens,
+        phn_ref1,
+        phn_ref2,
+        phn_ref1_lengths,
+        phn_ref2_lengths,
+    ):
+        if isinstance(encoder_out, list):
+            if encoder_out[0].shape[1] < phn_ref1.shape[1]:
+                phn_ref1 = phn_ref1[:, :encoder_out[0].shape[1]].contiguous()
+                phn_ref2 = phn_ref2[:, :encoder_out[0].shape[1]].contiguous()
+            elif encoder_out[0].shape[1] > phn_ref1.shape[1]:
+                encoder_out[0] = encoder_out[0][:, :phn_ref1.shape[1]].contiguous()
+                encoder_out[1] = encoder_out[1][:, :phn_ref1.shape[1]].contiguous()
+
+            if self.cut_begin_end:
+                assert encoder_out[0].shape[1]==phn_ref1.shape[1]==phn_ref2.shape[1], (encoder_out[0].shape,phn_ref1.shape,phn_ref2.shapie)
+                cut_begin = int(0.2 * phn_ref1_lengths.max())
+                cut_len = int(0.5 * phn_ref1_lengths.max())
+                cut_end = int(0.2 * phn_ref1_lengths.max()) + cut_len
+                encoder_out=[enc[:,cut_begin:cut_end] for enc in encoder_out]
+                encoder_out_lens= [torch.ones_like(enc) * int(cut_len) for enc in encoder_out_lens]
+                targets = [
+                    phn_ref1[:, cut_begin : cut_end],
+                    phn_ref2[:, cut_begin : cut_end],
+                ]
+                targets_lengths = encoder_out_lens
+
+
+            ys_hats = [
+                self.ce_lo(F.dropout(enc_out, p=self.dropout_rate)) for enc_out in encoder_out
+            ] # n_spk * (bs, lens, proj)
+            ys_hats_lengths = encoder_out_lens
+        else:
+            if encoder_out.shape[1] < phn_ref1.shape[1]:
+                phn_ref1 = phn_ref1[:, :encoder_out.shape[1]].contiguous()
+                phn_ref2 = phn_ref2[:, :encoder_out.shape[1]].contiguous()
+            elif encoder_out.shape[1] > phn_ref1.shape[1]:
+                encoder_out = encoder_out[:, :phn_ref1.shape[1]].contiguous()
+
+            ys_hats = [
+                lo(F.dropout(encoder_out, p=self.dropout_rate)) for lo in self.ce_lo
+            ]
+            ys_hats_lengths = [encoder_out_lens, encoder_out_lens]
+        return ys_hats, ys_hats_lengths
+
+
     def _calc_ce_loss(
         self,
         ys_hat: torch.Tensor,
@@ -510,12 +371,6 @@ class ESPnetHybridASRModel(AbsESPnetModel):
         ys_pad: torch.Tensor,
         ys_pad_lens: torch.Tensor,
     ):
-        # Calc CTC loss
-        # assert ys_hat.shape[:2] == ys_pad.shape[:2], f'{ys_hat.shape[:2]} vs. {ys_pad.shape[:2]}'
-        # bs, seq_len, _ = ys_hat.shape
-        # loss_ce = self.cross_entropy(ys_hat, ys_pad)  # (batch * seq_len)
-        # loss_ce = torch.sum(loss_ce.view(bs, seq_len), dim=1)  # (batch)
-
         bs, seq_len, o_dim = ys_hat.shape
         ys_hat = ys_hat.view(-1, o_dim)
         ys_pad = ys_pad.view(-1)
@@ -523,12 +378,3 @@ class ESPnetHybridASRModel(AbsESPnetModel):
         loss_ce = torch.sum(loss_ce.view(bs, seq_len), dim=1)  # (batch)
 
         return loss_ce
-
-    def _calc_rnnt_loss(
-        self,
-        encoder_out: torch.Tensor,
-        encoder_out_lens: torch.Tensor,
-        ys_pad: torch.Tensor,
-        ys_pad_lens: torch.Tensor,
-    ):
-        raise NotImplementedError
