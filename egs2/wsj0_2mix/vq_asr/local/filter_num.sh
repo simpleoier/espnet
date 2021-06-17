6965e348fd747932a6e9f1c004c316c81abbd103
shincling <shijing609@163.com>
2021-06-17 14:22:38 +0800

    Add the filter script

---
+++ egs2/wsj0_2mix/vq_asr/local/filter_num.sh
@@ -0,0 +1,6 @@
+../utils/filter_scp.pl ../dump/raw/tr_max_8k/utt2num_samples ../data/tr_max_8k/vq_spk1_dsf32 > ../dump/raw/tr_max_8k/vq_spk1_dsf32
+../utils/filter_scp.pl ../dump/raw/tr_max_8k/utt2num_samples ../data/tr_max_8k/vq_spk2_dsf32 > ../dump/raw/tr_max_8k/vq_spk2_dsf32
+
+../utils/filter_scp.pl ../dump/raw/cv_max_8k/utt2num_samples ../data/cv_max_8k/vq_spk1_dsf32 > ../dump/raw/cv_max_8k/vq_spk1_dsf32
+../utils/filter_scp.pl ../dump/raw/cv_max_8k/utt2num_samples ../data/cv_max_8k/vq_spk2_dsf32 > ../dump/raw/cv_max_8k/vq_spk2_dsf32
+
